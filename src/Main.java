5aea0adf41dc17255c707f61fad34ed4ae13694d
Nejra <skandronejra@gmail.com>
2023-03-24 18:40:58 +0100

    Bonus and super mega giga bonus added

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,30 @@
+import static java.lang.Math.*;
+
 public class Main {
     public static void main(String[] args) {
 
         // Zadatak 1
 
-        int spentMoney = -5;
-        int givenMoney = 100;
-        //double cashState = 100;
+        double spentMoney = 66;
+        double givenMoney = 100;
 
         int apoen20 = 20;
         int apoen10 = 10;
         int apoen2 = 2;
         int apoen1 = 1;
-
-        int cashState20 = 5;
-        int cashState10 = 5;
-        int cashState2 = 5;
-        int cashState1 = 5;
-
-        /*double apoen05 = 0.5;
+        double apoen05 = 0.5;
         double apoen02 = 0.2;
         double apoen01 = 0.1;
-        double apoen001 = 0.01;*/
+        double apoen001 = 0.01;
+
+        int cashState20 = 1;
+        int cashState10 = 1;
+        int cashState2 = 0;
+        int cashState1 = 2;
+        int cashState05 = 0;
+        int cashState02 = 0;
+        int cashState01 = 200;
+        int cashState001 =0;
 
         if ((spentMoney > 0)) {
 
@@ -28,7 +32,7 @@ public class Main {
                 System.out.println("You have too much money, you lucky person!");
             } else {
 
-                int change1 = givenMoney - spentMoney;
+                double change = (givenMoney - spentMoney);
                 int numApoen20 = 0;
                 int numApoen10 = 0;
                 int numApoen2 = 0;
@@ -38,172 +42,129 @@ public class Main {
                 int numApoen01 = 0;
                 int numApoen001 = 0;
 
+
+                double change1 = change;
+                System.out.println(change1);
+
                 if ((change1 / apoen20) >= 1) {
-                    numApoen20 = (change1 / apoen20);
+                    double numberOf20 = (change1/apoen20);
+                   int numberOf20Int = (int)floor(numberOf20);
 
-                    int change2 = change1 % apoen20;
-
-                    if ((change2 / apoen10) >= 1) {
-                        numApoen10 = change2 / apoen10;
-
-                        int change3 = change2 % apoen10;
-
-                        if ((change3 / apoen2) >= 1) {
-                            numApoen2 = change3 / apoen2;
-
-                            int change4 = change3 % apoen2;
-
-                            if ((change4 / apoen1) >= 1) {
-                                numApoen1 = change4 / apoen1;
-
-                               /* int change5 = change4 % apoen1;
-
-                                if ((change5 / apoen05) >= 1) {
-                                    numApoen05 = change5 / apoen05;
-
-                                    int change6 = change5 % apoen05;
-
-                                    if ((change6 / apoen01) >= 1) {
-                                        numApoen01 = change6 / apoen01;
-
-                                        int change7 = change6 % apoen01;
-
-                                        if ((change7 / apoen001) >= 1)
-                                        {
-                                            numApoen001 = change7 / apoen001;
-                                        }
-
-                                    }
-
-
-                                }*/
-                            }
-
-
-                        }
-
-
+                    if (numberOf20Int <= cashState20) {
+                        numApoen20 = numberOf20Int;
                     }
-
-                } else if ((change1 / apoen10) >= 1) {
-
-                    numApoen10 = change1 / apoen10;
-
-                    int change3 = change1 % apoen10;
-
-                    if ((change3 / apoen2) >= 1) {
-                        numApoen2 = change3 / apoen2;
-
-                        int change4 = change3 % apoen2;
-
-                        if ((change4 / apoen1) >= 1) {
-                            numApoen1 = change4 / apoen1;
-
-                               /* int change5 = change4 % apoen1;
-
-                                if ((change5 / apoen05) >= 1) {
-                                    numApoen05 = change5 / apoen05;
-
-                                    int change6 = change5 % apoen05;
-
-                                    if ((change6 / apoen01) >= 1) {
-                                        numApoen01 = change6 / apoen01;
-
-                                        int change7 = change6 % apoen01;
-
-                                        if ((change7 / apoen001) >= 1)
-                                        {
-                                            numApoen001 = change7 / apoen001;
-                                        }
-
-                                    }
-
-
-                                }*/
-                        }
-
-
+                    else {
+                        numApoen20 = cashState20;
                     }
-
-
-                } else if ((change1 / apoen2) >= 1) {
-                    numApoen2 = change1 / apoen2;
-
-                    int change4 = change1 % apoen2;
-
-                    if ((change4 / apoen1) >= 1) {
-                        numApoen1 = change4 / apoen1;
-
-                               /* int change5 = change4 % apoen1;
-
-                                if ((change5 / apoen05) >= 1) {
-                                    numApoen05 = change5 / apoen05;
-
-                                    int change6 = change5 % apoen05;
-
-                                    if ((change6 / apoen01) >= 1) {
-                                        numApoen01 = change6 / apoen01;
-
-                                        int change7 = change6 % apoen01;
-
-                                        if ((change7 / apoen001) >= 1)
-                                        {
-                                            numApoen001 = change7 / apoen001;
-                                        }
-
-                                    }
-
-
-                                }*/
-                    }
-
-                } else if ((change1 / apoen1) >= 1) {
-                    numApoen1 = change1 / apoen1;
-
-                    /*int change4 = change1 % apoen2;
-
-                    if ((change4 / apoen2) >= 1) {
-                        numApoen1 = change4 / apoen1;
-
-                               int change5 = change4 % apoen1;
-
-                                if ((change5 / apoen05) >= 1) {
-                                    numApoen05 = change5 / apoen05;
-
-                                    int change6 = change5 % apoen05;
-
-                                    if ((change6 / apoen01) >= 1) {
-                                        numApoen01 = change6 / apoen01;
-
-                                        int change7 = change6 % apoen01;
-
-                                        if ((change7 / apoen001) >= 1)
-                                        {
-                                            numApoen001 = change7 / apoen001;
-                                        }
-
-                                    }
-
-
-                                }*/
+                    change1 = change1 - (numApoen20 * apoen20);
                 }
 
-                System.out.println("20 KM: " + numApoen20);
-                System.out.println("10 KM: " + numApoen10);
-                System.out.println("2 KM: " + numApoen2);
-                System.out.println("1 KM: " + numApoen1);
-                System.out.println("0.5 KM: " + numApoen05);
-                System.out.println("0.1 KM: " + numApoen01);
-                System.out.println("0.01 KM: " + numApoen001);
+
+                if ((change1 / apoen10) >= 1) {
+                    double numberOf10 = (change1/apoen10);
+                    int numberOf10Int = (int)round(numberOf10);
+                    if (numberOf10Int <= cashState10) {
+                        numApoen10 = numberOf10Int;
+                    }
+                    else {
+                        numApoen10 = cashState10;
+                    }
+                    change1 = change1 - (numApoen10 * 10);
+                }
+
+                if ((change1 / apoen2) >= 1) {
+                    double numberOf2 = (change1/apoen2);
+                    int numberOf2Int = (int)floor(numberOf2);
+                    if (numberOf2Int <= cashState2) {
+                        numApoen2 = numberOf2Int;
+
+                    }
+                    else {
+                        numApoen2 = cashState2;
+                    }
+                    change1 = change1 - (numApoen2 * apoen2);
+
+                }
+
+                if ((change1 / apoen1) >= 1) {
+                    double numberOf1 = (change1/apoen1);
+                    int numberOf1Int = (int)floor(numberOf1);
+                    if (numberOf1Int <= cashState1) {
+                        numApoen1 = numberOf1Int;
+                    }
+                    else {
+                        numApoen1 = cashState1;
+                    }
+                    change1 = change1 - (numApoen1 * apoen1);
+                }
+
+                if ((change1 / apoen05) >= 1) {
+                    double numberOf05 = (change1/apoen05);
+                    int numberOf05Int = (int)floor(numberOf05);
+                    if (numberOf05 <= cashState05) {
+                        numApoen05 = numberOf05Int;
+                    }
+                    else {
+                        numApoen05 = cashState05;
+                    }
+                    change1 = change1 - (numApoen05 * apoen05);
+                }
+
+                if ((change1 / apoen02) >= 1) {
+                    double numberOf02 = (change1/apoen02);
+                    int numberOf02Int = (int)floor(numberOf02);
+                    if (numberOf02 <= cashState02) {
+                        numApoen02 = numberOf02Int;
+                    }
+                    else {
+                        numApoen02 = cashState02;
+                    }
+                    change1 = change1 - (numApoen02 * apoen02);
+                }
+
+                if ((change1 / apoen01) >= 1) {
+                    double numberOf01 = (change1/apoen01);
+                    int numberOf01Int = (int)floor(numberOf01);
+                    if (numberOf01Int <= cashState01) {
+                        numApoen01 = numberOf01Int;
+                    }
+                    else {
+                        numApoen01 = cashState01;
+                    }
+                    change1 = change1 - (numApoen01 * apoen01);
+                }
+
+                if ((change1 / apoen001) >= 1) {
+                    double numberOf001 = (change1/apoen001);
+                    int numberOf001Int = (int)floor(numberOf001);
+                    if (numberOf001 <= cashState001) {
+                        numApoen001 = numberOf001Int;
+                    }
+                    else {
+                        numApoen001 = cashState001;
+                    }
+                    change1 = change1 - (numApoen001 * apoen001);
+                }
+
+                if (change == change1) {
+                    System.out.println("SORRY THE CASH REGISTER DOES NOT HAVE THE REQUIRED AMOUNT OF MONEY");
+                }
+
+                    System.out.println("20 KM: " + numApoen20);
+                    System.out.println("10 KM: " + numApoen10);
+                    System.out.println("2 KM: " + numApoen2);
+                    System.out.println("1 KM: " + numApoen1);
+                    System.out.println("0.5 KM: " + numApoen05);
+                    System.out.println("0.1 KM: " + numApoen01);
+                    System.out.println("0.01 KM: " + numApoen001);
+
             }
-        }
-         else {
-                System.out.println("You didn't buy anything, did you?");
+        } else {
+            System.out.println("You didn't buy anything, did you?");
             if (spentMoney < 0) {
                 System.out.println("“Did you maybe break something?");
+            }
         }
-    }
-
 
 
         // Zadatak 2
